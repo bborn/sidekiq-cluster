bc627df1d0bddb790f000dbd8d20c26835c23cad
kalle saas <kalle@easypep.de>
2019-07-19 19:53:30 +0200

    inverse vs reverse ruby method

--- lib/sidekiq/cluster/memory/total.rb
+++ lib/sidekiq/cluster/memory/total.rb
@@ -10,7 +10,7 @@ module Sidekiq
           total_ram_pct = worker_pool.map(&:memory_used_pct).sum
           worker_pool.cli.info("total RAM used by workers is #{'%.2f%%' % total_ram_pct}")
           if total_ram_pct > config.max_memory_percent
-            worker_pool.sort_by(&:memory_used_pct).inverse[0..1]
+            worker_pool.sort_by(&:memory_used_pct).reverse[0..1]
           end || []
         end
       end
