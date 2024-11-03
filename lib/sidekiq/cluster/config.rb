c384777333e73409cc55da02c92a260bd5a52584
Bruno Bornsztein <bruno.bornsztein@gmail.com>
2024-11-03 06:59:21 -0600

    fix config

--- lib/sidekiq/cluster/config.rb
+++ lib/sidekiq/cluster/config.rb
@@ -5,34 +5,34 @@ require_relative "version"
 module Sidekiq
   module Cluster
     class Config
-      MEMORY_STRATEGIES = %i(total individual)
+      MEMORY_STRATEGIES = %i[total individual]
       ARGV_SEPARATOR = "--".freeze
 
       extend ::Dry::Configurable
 
-      setting :spawn_block, ->(worker) {
-                require "sidekiq"
-                require "sidekiq/cli"
+      setting :spawn_block, constructor: ->(worker) {
+                                           require "sidekiq"
+                                           require "sidekiq/cli"
 
-                process_argv = worker.config.worker_argv.dup
-                process_argv << "--tag" << "sidekiq.#{worker.config.name}.#{worker.index + 1}"
+                                           process_argv = worker.config.worker_argv.dup
+                                           process_argv << "--tag" << "sidekiq.#{worker.config.name}.#{worker.index + 1}"
 
-                cli = Sidekiq::CLI.instance
-                cli.parse %w(bundle exec sidekiq) + process_argv
-                cli.run
-              }
+                                           cli = Sidekiq::CLI.instance
+                                           cli.parse %w[bundle exec sidekiq] + process_argv
+                                           cli.run
+                                         }
 
-      setting :name, "default"
-      setting :process_count, Etc.nprocessors - 1
-      setting :max_memory_percent, MAX_RAM_PCT
-      setting :memory_strategy, :total # also supported :individual
+      setting :name, default: "default"
+      setting :process_count, default: Etc.nprocessors - 1
+      setting :max_memory_percent, default: MAX_RAM_PCT
+      setting :memory_strategy, default: :total # also supported :individual
       setting :logfile
-      setting :work_dir, Dir.pwd
-      setting :cluster_argv, []
-      setting :worker_argv, []
-      setting :debug, false
-      setting :quiet, false
-      setting :help, false
+      setting :work_dir, default: Dir.pwd
+      setting :cluster_argv, default: []
+      setting :worker_argv, default: []
+      setting :debug, default: false
+      setting :quiet, default: false
+      setting :help, default: false
 
       def self.split_argv(argv)
         configure do |c|
--- sidekiq-cluster.gemspec
+++ sidekiq-cluster.gemspec
@@ -1,32 +1,30 @@
-# coding: utf-8
-
 Gem::Specification.new do |spec|
-  spec.name          = 'sidekiq-cluster'
-  spec.version       = '0.1.2'
-  spec.authors       = ['Konstantin Gredeskoul']
-  spec.email         = ['kigster@gmail.com']
+  spec.name = "sidekiq-cluster"
+  spec.version = "0.1.2"
+  spec.authors = ["Konstantin Gredeskoul"]
+  spec.email = ["kigster@gmail.com"]
 
-  spec.summary       = 'Foo Bar'
-  spec.description   = 'Foo Bar'
-  spec.homepage      = 'https://github.com/kigster/sidekiq-cluster'
-  spec.license       = 'MIT'
+  spec.summary = "Foo Bar"
+  spec.description = "Foo Bar"
+  spec.homepage = "https://github.com/kigster/sidekiq-cluster"
+  spec.license = "MIT"
 
   spec.files = `git ls-files -z`.split("\x0").reject do |f|
     f.match(%r{^(test|spec|features)/})
   end
-  spec.bindir        = 'exe'
-  spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
-  spec.require_paths = ['lib']
+  spec.bindir = "exe"
+  spec.executables = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
+  spec.require_paths = ["lib"]
 
-  spec.add_dependency 'colored2' 
-  spec.add_dependency 'sidekiq' 
-  spec.add_dependency 'dry-configurable'
-  spec.add_dependency 'state_machines'
+  spec.add_dependency "colored2"
+  spec.add_dependency "sidekiq"
+  spec.add_dependency "dry-configurable"
+  spec.add_dependency "state_machines"
 
-  spec.add_development_dependency 'rspec', '~> 3.5.0'
-  spec.add_development_dependency 'rspec-its'
-  spec.add_development_dependency 'bundler', '~> 1.15'
-  spec.add_development_dependency 'rake', '~> 10.0'
-  spec.add_development_dependency 'simplecov'
-  spec.add_development_dependency 'yard'
+  spec.add_development_dependency "rspec", "~> 3.5.0"
+  spec.add_development_dependency "rspec-its"
+  spec.add_development_dependency "bundler"
+  spec.add_development_dependency "rake", "~> 10.0"
+  spec.add_development_dependency "simplecov"
+  spec.add_development_dependency "yard"
 end
